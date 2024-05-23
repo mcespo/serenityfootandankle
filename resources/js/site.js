6029abf976a9db331ac929fb22abe752291adf69
Marvin Céspedes <mcespo@gmail.com>
2024-05-22 23:33:22 -0500

    alpine and nav

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,10 @@
 // This is all you.
+// import focus from "@alpinejs/focus";
+import Alpine from "alpinejs";
+// import intersect from "@alpinejs/intersect";
+
+// Alpine.plugin(focus);
+// Alpine.plugin(intersect);
+window.Alpine = Alpine;
+
+Alpine.start();
