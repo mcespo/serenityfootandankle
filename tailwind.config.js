c2666b86d0eb9d9feef471b71c99f0b784d2d557
Marvin Céspedes <mcespo@gmail.com>
2024-06-10 23:54:03 -0500

    mobile nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,16 @@ export default {
         "./content/**/*.md",
     ],
     theme: {
+        screens: {
+            "3xs": "22.5rem", // 360px
+            "2xs": "25rem", // 400px
+            xs: "30rem", // 480px
+            sm: "40rem", // 640px
+            md: "48rem", // 768px
+            lg: "64rem", // 1024px
+            xl: "80rem", // 1280px
+            "2xl": "96rem", // 1536px
+        },
         extend: {
             colors: {
                 blue: {
